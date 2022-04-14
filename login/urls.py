0f8bf01498a123a4cb373b4aa3275940d53fb459
vika-ch23 <chigirina.vika@mail.ru>
2022-04-14 17:39:00 +0300

    authrization

--- login/urls.py
+++ login/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('api/account/', include('login.api.urls', 'login_api'))
+    path('', include('login.api.urls', 'login_api'))
 ]
